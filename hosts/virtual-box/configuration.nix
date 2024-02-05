bdfa02e6aec3d0c5dae504e8a6132e4d005bbcd8
Dominik M <d.meyer99@hotmail.com>
2024-02-05 23:07:30 +0000

    add neofetch

--- hosts/virtual-box/configuration.nix
+++ hosts/virtual-box/configuration.nix
@@ -69,6 +69,7 @@
 
   # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
+    neofetch
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     wget
