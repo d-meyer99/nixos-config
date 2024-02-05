ec631eb832c1ae91298fd0d9a35a68961b7f847e
Dominik M <d.meyer99@hotmail.com>
2024-02-05 15:27:37 +0000

    add keychain

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -35,6 +35,7 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
     pkgs.oh-my-posh
+    pkgs.keychain
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
